7a53be673da1f02c1cec52975203b9509da8db3a
mizan <dewan.mizanur911@gmail.com>
2022-03-19 15:01:31 +0600

    fallback false , true and blocking implemented on postId page

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -1,6 +1,13 @@
+import {useRouter} from 'next/router';
 import React from 'react';
 
 const Post = ({post}) => {
+    // For fallback is true 
+    const router = useRouter();
+    if(router.isFallback){
+        return <h3>Loading...</h3>
+    }
+
     return (
         <div>
             <h3>post no. {post.id}</h3>
@@ -29,7 +36,9 @@ export async function getStaticPaths(){
         //     }
         // ],
         paths,
-        fallback : false
+        // fallback : false
+        fallback : true
+        // fallback : 'blocking'
     }
 }
 
@@ -38,6 +47,13 @@ export async function getStaticProps(context) {
     const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.postId}`);
     const data = await res.json();
 
+    if(!data.id){
+        return {
+            notFound : true
+        }
+    }
+
+    console.log(`Generating page for /posts/${params.postId}`);
     return {
         props : {
             post : data
